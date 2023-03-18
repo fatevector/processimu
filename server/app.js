82d4ac8d5686459d286125b7105c5455045e14bb
Иван Петров <fatevector@yandex.ru>
2023-03-18 20:48:05 +0300

    init project

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const config = require("./config.json")
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/api", routes);
+
+const PORT = config.get("port") ?? 8080;
+
+const start = async () => {
+    try {
+        mongoose.set("strictQuery", true);
+        await mongoose.connect(config.get("mongoUri"));
+        console.log(chalk.green("MongoDB is connected"));
+        app.listen(PORT, () => {
+            console.log(
+                chalk.green(`Server has been started on port ${PORT}...`)
+            );
+        });
+    } catch (error) {
+        console.log(chalk.red(error.message));
+        process.exit(1);
+    }
+};
+
+start();
