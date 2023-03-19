597bb5a2f7871525e840530ff6ccec5fa4c10936
Иван Петров <fatevector@yandex.ru>
2023-03-20 00:53:41 +0300

    add logOut logic

--- client/src/app/components/layout/logOut.jsx
+++ client/src/app/components/layout/logOut.jsx
@@ -1,7 +1,19 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { logOut } from "../../store/auth";
+import Loader from "../common/loader";
+
 const LogOut = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(logOut());
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <div className="m-3">
-            <h1>Layout выхода</h1>
+            <Loader />
         </div>
     );
 };
