a881d27470d730733b5df28d8102a0754764f25e
Иван Петров <fatevector@yandex.ru>
2023-05-06 02:14:55 +0300

    add draggable grid

--- client/src/app/components/pages/modelCreationPage.jsx
+++ client/src/app/components/pages/modelCreationPage.jsx
@@ -1,7 +1,19 @@
+import Grid from "../ui/grid";
+
 const ModelCreationPage = () => {
+    const n = 15;
+    const data = new Array(n);
+    data.fill(null);
+    data.forEach((_, i) => {
+        const row = new Array(n);
+        row.fill(null);
+        data[i] = row;
+    });
+
     return (
         <div className="m-3">
             <h3>Создание модели</h3>
+            <Grid data={data} />
         </div>
     );
 };
---
+++ client/src/app/components/ui/grid.jsx
@@ -0,0 +1,76 @@
+const Grid = ({ data }) => {
+    let dragObject = {};
+
+    document.onmousedown = e => {
+        if (e.button !== 0) {
+            // если клик не левой кнопкой мыши
+            return; // то он не запускает перенос
+        }
+
+        const elem = e.target.closest(".draggable");
+
+        if (!elem) return; // не нашли, клик вне draggable-объекта
+
+        // запомнить переносимый объект
+        dragObject.elem = elem;
+
+        // запомнить координаты, с которых начат перенос объекта
+        dragObject.downX = e.pageX;
+        dragObject.downY = e.pageY;
+        dragObject.initIndentX = +dragObject.elem.style.left.slice(0, -2) || 0;
+        dragObject.initIndentY = +dragObject.elem.style.top.slice(0, -2) || 0;
+
+        // задаем доступные границы перетаскивания
+        const field = e.target.closest(".droppable");
+        const fieldWidth = field.getBoundingClientRect().width;
+        const fieldHeight = field.getBoundingClientRect().height;
+        dragObject.minLeft = fieldWidth - data[0].length * 50;
+        dragObject.minTop = fieldHeight - data.length * 50;
+        dragObject.maxLeft = 0;
+        dragObject.maxTop = 0;
+    };
+
+    document.onmousemove = function (e) {
+        if (!dragObject.elem) return; // элемент не зажат
+
+        const moveX = e.pageX - dragObject.downX;
+        const moveY = e.pageY - dragObject.downY;
+
+        // отображаем перенос объекта при каждом движении мыши
+        // не позволяем перемещать за границы поля
+        let newLeft = dragObject.initIndentX + moveX;
+        if (newLeft < dragObject.minLeft) newLeft = dragObject.minLeft;
+        if (newLeft > dragObject.maxLeft) newLeft = dragObject.maxLeft;
+        dragObject.elem.style.left = newLeft + "px";
+
+        let newTop = dragObject.initIndentY + moveY;
+        if (newTop < dragObject.minTop) newTop = dragObject.minTop;
+        if (newTop > dragObject.maxTop) newTop = dragObject.maxTop;
+        dragObject.elem.style.top = newTop + "px";
+
+        return false;
+    };
+
+    document.onmouseup = function (e) {
+        // очистим "состояние переноса" dragObject
+        dragObject = {};
+    };
+
+    return (
+        <div className="gridWrap droppable">
+            <div className="grid draggable">
+                {data.map(row => (
+                    <div className="gridRow" key={Math.random()}>
+                        {row.map(cell => (
+                            <div className="gridCell" key={Math.random()}>
+                                {cell}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Grid;
