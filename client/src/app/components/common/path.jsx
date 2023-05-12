b00a73ed976551f90724d2fa68219ffeeb34fba5
Иван Петров <fatevector@yandex.ru>
2023-05-12 13:58:49 +0300

    replaced linear connections with bezier curves

--- client/src/app/components/common/path.jsx
+++ client/src/app/components/common/path.jsx
@@ -5,8 +5,12 @@ const Path = ({ path, onSelected, selected }) => {
     return (
         <>
             <path
-                // d={`M${a.left},${a.top} C50,${a.top} 50 ${b.top} ${b.left} ${b.top}`}
-                d={`M${a.left},${a.top} L${b.left},${b.top}`}
+                d={`M${a.left},${a.top} C${a.left + (b.left - a.left) / 2},${
+                    a.top
+                } ${a.left + (b.left - a.left) / 2},${b.top} ${b.left},${
+                    b.top
+                }`}
+                // d={`M${a.left},${a.top} L${b.left},${b.top}`}
                 className={
                     "centerPath" + (selected?.id === path.id ? " selected" : "")
                 }
@@ -17,6 +21,17 @@ const Path = ({ path, onSelected, selected }) => {
                     })
                 }
             ></path>
+            {/* Раскомментировать, чтобы увидеть контрольные точки */}
+            {/* <circle
+                cx={a.left + (b.left - a.left) / 2}
+                cy={a.top}
+                r={"0.5"}
+            ></circle>
+            <circle
+                cx={a.left + (b.left - a.left) / 2}
+                cy={b.top}
+                r={"0.5"}
+            ></circle> */}
         </>
     );
 };
